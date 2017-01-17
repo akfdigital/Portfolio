b567a90110a9b3b96fe1d2061832c9fbad012615
“Ali <truehueali@gmail.com>
2017-01-17 11:38:39 -0500

    added about me section

--- public/js/main.js
+++ public/js/main.js
@@ -5,21 +5,28 @@ $( document ).ready(function() {
 	$('#element').typeIt({
      	speed: 50,
      	breakLines: true,
-     	autoStart: false
+     	autoStart: false, 
+     	loop: true, 
+     	loopDelay: 750
 	})
 	.tiType('Hi, I\'m Ali FitzGerald')
 	.tiPause(500)
-	.tiDelete(14)
-	.tiType('AKFDigital, ')
+	.tiDelete(22)
+	.tiType('Welcome! ')
 	.tiBreak() .tiPause(750)
-	.tiType('Happy you\'re here.')
-	.tiBreak() .tiPause(750)
-	.tiType(' Maybe I can help you')
-	.tiSettings({speed: 700})
-	.tiType('...')
-	.tiPause(750)
 	.tiSettings({speed: 50})
 	.tiDelete()
-	.tiType('Thanks!');
+
+
+
+	var controller = new ScrollMagic.Controller();
+
+// create a scene
+new ScrollMagic.Scene({
+        duration: 100,    // the scene should last for a scroll distance of 100px
+        offset: 50        // start this scene after scrolling for 50px
+    })
+    .setPin("") // pins the element for the the scene's duration
+    .addTo(controller); // assign the scene to the controller
 
 });
