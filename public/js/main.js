9f233b14b8546902d52a1af2c97fa48c14dafea4
“Ali <truehueali@gmail.com>
2017-02-04 10:43:46 -0500

    about section added

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,18 @@
 
 $( document ).ready(function() {
-    console.log( "ready!" );
 
-	$('#element').typeIt({
-     	speed: 50,
-     	breakLines: true,
-     	autoStart: false, 
-     	loop: true, 
-     	loopDelay: 750
-	})
-	.tiType('Hi, I\'m Ali FitzGerald')
-	.tiPause(500)
-	.tiDelete(22)
-	.tiType('Welcome! ')
-	.tiBreak() .tiPause(750)
-	.tiSettings({speed: 50})
-	.tiDelete()
+  // init controller
+  var controller = new ScrollMagic.Controller({vertical: true});
 
+  // build tween
+  var tween = TweenMax.to("#target", 0.5, {backgroundColor: "green", width: "+=400"});
 
+  // build scene
+  var scene = new ScrollMagic.Scene({triggerElement: "#trigger", duration: 500})
+          .setTween(tween)
+          .addIndicators() // add indicators (requires plugin)
+          .addTo(controller);
 
-	var controller = new ScrollMagic.Controller();
-
-// create a scene
-new ScrollMagic.Scene({
-        duration: 100,    // the scene should last for a scroll distance of 100px
-        offset: 50        // start this scene after scrolling for 50px
-    })
-    .setPin("") // pins the element for the the scene's duration
-    .addTo(controller); // assign the scene to the controller
+    
 
 });
